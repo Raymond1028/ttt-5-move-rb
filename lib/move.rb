09aa99c3f760dbcae8be63692a6c6224846a8d15
Ray's Git <lancelot53.ra@gmail.com>
2021-10-03 14:24:51 -0400

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,5 +1,10 @@
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board(board)
+
+
+#board = [" "," "," "," "," "," "," "," "," "]
+#puts " Please enter the positon on the board you'd like to fill from 1 -9"
+#input = gets.strip
+
+def display_board(board) 
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -10,11 +15,13 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(input)
-input.to_i - 1
+ input.to_i - 1
 end
 
-def move(board,input,character = "X")
-  board[input] = character
+def move(board,index,character = "X")
+  board[index] = character
 end
 
-display_board(board)
+# index = input_to_index(input)
+# move(board,index,character = "X")
+# display_board(board)
